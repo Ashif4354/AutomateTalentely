2474f341beff221eeda214056c6fec1a3323eb29
A Ashif <53007623+Ashif4354@users.noreply.github.com>
2023-09-13 09:25:38 +0000

    changed spelling

--- a.py
+++ a.py
@@ -2,5 +2,5 @@ import logger
 from selenium import webdriver
 
 browser = webdriver.Edge()
-browser.get('https://www.google.com')
+browser.get('https://www.googl.com')
 logger.logger('20cs008@kcgcollege.com', '1.1').report_exception((1,2), 'a.py', 'hello exception', browser)
